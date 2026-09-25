ae38ba9a4ea1e2b5eea1b5b384b87d430d6dcd2e
kruthir-sys <kruthir@goa.bits-pilani.ac.in>
2026-09-25 10:48:15 +0530

    Update app_session_form.py

--- app_session_form.py
+++ app_session_form.py
@@ -126,7 +126,7 @@ if _session_schedule_env:
     SESSION_SCHEDULE = _json.loads(_session_schedule_env)
 else:
     SESSION_SCHEDULE = {
-        "L1/P1(RAUNAK)":  {"days": ["Mon", "Wed", "Fri","Thu"], "start": "12:00", "end": "19:00"},
+        "L1/P1(RAUNAK)":  {"days": ["Mon", "Wed", "Fri","Thu"], "start": "10:49", "end": "19:00"},
         "L2/P2(ABHISEK)": {"days": ["Tue", "Thu", "Fri"], "start": "14:30", "end": "16:00", "end_buffer": 0},
         "L3/P3(SANDESH)": {"days": ["Tue", "Thu", "Fri"], "start": "16:00", "end": "17:00", "start_buffer": 0},
     }
